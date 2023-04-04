616450c66f28863115a58c7c7583b51f7502cfa1
Duy <duytv.dev@gmail.com>
2023-04-04 15:15:50 +0700

    done

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 import express from "express";
-import routerUser from "./router/user.js";
-import dotenv from "dotenv";
-// const port= 8000;
-dotenv.config();
+import productRouter from "./routes/product.js";
+import authRouter from "./routes/auth.js"
+import mongoose from "mongoose";
+import cors from "cors";
+
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
-app.use("/api", routerUser);
+app.use("/api", productRouter);
+app.use("/api", authRouter);
 
+mongoose.connect("mongodb://127.0.0.1:27017/we17304");
 
 export const viteNodeApp = app;
-
-// app.listen(port, () => {
-//   console.log(`Server is running on ${port}`);
-// });
