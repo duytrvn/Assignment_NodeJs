168bac1c27c767f163603440e7cffc3741c39727
Duy <duytv.dev@gmail.com>
2023-04-08 13:56:04 +0700

    Router Categories

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { create, get } from "../controllers/category";
+import { create, get , getAll , update , remove} from "../controllers/category";
 import { checkPermission } from "../middlewares/checkPermission";
 
 const router = express.Router();
+router.get("/categories", getAll);
 router.get("/categories/:id", get);
 router.post("/categories", checkPermission, create);
+router.put("/categories/:id", checkPermission, update);
+router.delete("/categories/:id", checkPermission, remove);
 
 export default router;
